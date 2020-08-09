1342ad1d526f86df7dcfbe6cf0078205dd42d59a
Kanchan <kanchankandage@gmail.com>
2020-08-09 16:20:04 -0400

    DOM exercise with menu button

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+const menuList = document.querySelector(".menuList");
+const menuContainer = document.querySelector(".menuContainer");
+const letters = document.querySelector(".letters");
+const numbers = document.querySelector(".numbers");
+
+menuContainer.addEventListener("click", (e) => {
+	if (e.target.classList.contains("menuBtn")) {
+		menuList.classList.toggle("hidden");
+	}
+
+	if (e.target.classList.contains("letterBtn")) {
+		e.target.style.color = "blue";
+		letters.classList.remove("hidden");
+		numbers.classList.add("hidden");
+	} else if (e.target.classList.contains("numBtn")) {
+		e.target.style.color = "blue";
+		letters.classList.add("hidden");
+		numbers.classList.remove("hidden");
+	}
+});
